1560266cbd42f37388916e60d719cb5614355af2
gingus55 <gingus55@hotmail.com>
2021-11-12 16:59:49 +0000

    pseudo code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("howdy planet");
+
+// import inquirer
+const inquirer = require("inquirer");
+
+// set questions
+
+// get answers
+
+// handle answers
+
+// write README
+
+// start program
