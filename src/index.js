58ed1224ce5564746b19f1839418953a53bde5cf
gingus55 <gingus55@hotmail.com>
2021-11-13 14:17:28 +0000

    links all working now ids

--- src/index.js
+++ src/index.js
@@ -25,8 +25,19 @@ const questions = [
   },
   {
     type: "input",
+    name: "installation",
+    message: "How do I install your application?",
+  },
+  {
+    type: "confirm",
+    name: "testIncluded",
+    message: "Do you test?",
+  },
+  {
+    type: "input",
     name: "tests",
     message: "How do I test your application?",
+    when: (answers) => answers.testIncluded,
   },
   {
     type: "list",
@@ -72,45 +83,60 @@ const writeReadme = (answers) => `
 
 ## Table of contents
 1. [Description](#description)
-2. [User Story](#userstory)
-3. User Guidance
-    1. [Contribution Guidelines](#contribution)
+2. [User Story](#user-story)
+3. [User Guidance](#guidance)
+    1. [Installation Instructions](#installation)
     2. [Test Instructions](#test)
+    3. [Contribution Guidelines](#contribution)
+
 4. [Questions](#questions)
 
-<a name="description"></a>
+<a id="description"></a>
 ### Description of application 
 ${answers.description}
 
-<a name="userstory"></a>
+<a id="user-story"></a>
 ### User Story 
-${answers.user}
 
+${answers.usage}
+
+
+<a id="guidance"></a>
 ### User guidance
-<a name="contribution"></a>
-#### Contribution Guidelines 
-${answers.contribution}
-<a name="test"></a>
+
+<a id="installation"></a>
+#### Installation Instructions 
+
+${answers.installation}
+
+<a id="test"></a>
 #### Test Instructions 
+
 ${answers.tests}
 
-<a name="questions"></a>
+<a id="contribution"></a>
+#### Contribution Guidelines 
+
+${answers.contribution}
+
+
+<a id="questions"></a>
 ### Questions 
 Should you have any questions regarding this project please reach me via email or via my GitHub using the details below:
 
 -Email address: ${answers.email}
 
 -GitHub username : ${answers.githubName}
+
 `;
 
 // start program
 const start = async () => {
   // prompt questions and get answers
   const readmeAnswers = await inquirer.prompt(questions);
+  console.log(readmeAnswers);
 
   const readmeDoc = writeReadme(readmeAnswers);
-
-  //   console.log(readmeDoc);
 
   // write to file with data and path
   utilities.writeToFile("generatedREADME.md", readmeDoc);
