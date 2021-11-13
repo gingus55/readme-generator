7f75d7aa711ba086d76ef9e5f68c818e14b80e1b
gingus55 <gingus55@hotmail.com>
2021-11-13 15:58:33 +0000

    made changes when tests and installation not required

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // import inquirer
 const inquirer = require("inquirer");
 const utilities = require("./utilities");
+const writeReadme = require("./readme");
+
 // set questions
 const questions = [
   {
@@ -24,14 +26,20 @@ const questions = [
     message: "What are your contribution guidelines?",
   },
   {
+    type: "confirm",
+    name: "installationIncluded",
+    message: "Do I need to install your application?",
+  },
+  {
     type: "input",
     name: "installation",
     message: "How do I install your application?",
+    when: (answers) => answers.installationIncluded,
   },
   {
     type: "confirm",
     name: "testIncluded",
-    message: "Do you test?",
+    message: "Are there tests for this application?",
   },
   {
     type: "input",
@@ -42,7 +50,7 @@ const questions = [
   {
     type: "list",
     name: "license",
-    message: "What licenses is required for this project?",
+    message: "What license is required for this project?",
     choices: [
       {
         name: "MIT",
@@ -73,62 +81,6 @@ const questions = [
     message: "What is your email address?",
   },
 ];
-
-// write README - include readme markup
-
-const writeReadme = (answers) => `
-
-# ${answers.title}
-![${answers.license}](https://img.shields.io/static/v1?label=${answers.license}&message=License&color=green)
-
-## Table of contents
-1. [Description](#description)
-2. [User Story](#user-story)
-3. [User Guidance](#guidance)
-    1. [Installation Instructions](#installation)
-    2. [Test Instructions](#test)
-    3. [Contribution Guidelines](#contribution)
-
-4. [Questions](#questions)
-
-<a id="description"></a>
-### Description of application 
-${answers.description}
-
-<a id="user-story"></a>
-### User Story 
-
-${answers.usage}
-
-
-<a id="guidance"></a>
-### User guidance
-
-<a id="installation"></a>
-#### Installation Instructions 
-
-${answers.installation}
-
-<a id="test"></a>
-#### Test Instructions 
-
-${answers.tests}
-
-<a id="contribution"></a>
-#### Contribution Guidelines 
-
-${answers.contribution}
-
-
-<a id="questions"></a>
-### Questions 
-Should you have any questions regarding this project please reach me via email or via my GitHub using the details below:
-
--Email address: ${answers.email}
-
--GitHub username : ${answers.githubName}
-
-`;
 
 // start program
 const start = async () => {
